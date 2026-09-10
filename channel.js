cd3da7649c0e96b02d0b468f059236509368ca9f
svimran46 <svimranmy@gmail.com>
2026-09-10 20:36:52 +0800

    Replace channel.js with updated sports channels list

--- channel.js
+++ channel.js
@@ -1,41 +1,98 @@
 const channels = [
-    {
-        name: "BBC Two",
-        category: "entertainment",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/BBC_Two_logo_2021.svg/1200px-BBC_Two_logo_2021.svg.png",
-        url: "http://193.239.187.97/BBC2/index.m3u8"
-    },
-    {
-        name: "Sky Sports F1 UHD",
-        category: "sports",
-        logo: "https://i.ibb.co/1tmrNyj9/Sky-Sport-F1-UHD.png",
-        url: "http://czstream.com:826/live/LouCarey/KYsHfE1YLU/118946.m3u8"
-    },
-    {
-        name: "Hub Premier 4 FHD",
-        category: "sports",
-        logo: "https://r2.thesportsdb.com/images/media/channel/logo/emwu7x1659622075.png",
-        url: "http://tbtv.me:2095/live/383301917192/909365136076/1309858.m3u8"
-    },
-    {
-        name: "FanDuel Sports Florida",
-        category: "sports",
-        logo: "https://i.imgur.com/S5cqDnq.png",
-        url: "http://raccoon.bz:8080/live/leo24/leo24/44980.m3u8"
-    },
-    {
-        name: "FanDuel Sports Midwest",
-        category: "sports",
-        logo: "https://i.ibb.co/5W31SKh5/Fan-Duel-Midwest.png",
-        url: "http://raccoon.bz:8080/live/leo24/leo24/44987.m3u8"
-    },
-    {
-        name: "BeIN Sports USA",
-        category: "sports",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/BeIN_Sports_logo.svg/1200px-BeIN_Sports_logo.svg.png",
-        // IMPORTANT: For BeIN, you must extract the raw .m3u8 link using F12 -> Network.
-        // The iframe link will NOT work in this player.
-        url: "YOUR_EXTRACTED_BEIN_M3U8_LINK_HERE"
-    }
-    // Add more channels here...
+  {
+    "name": "ACC Network",
+    "logo": "https://theacc.com/images/2018/11/30/ACCN_Launch.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/accn-usa"
+  },
+  {
+    "name": "beIN Sports",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/BeIN_Sports_logo.svg/1200px-BeIN_Sports_logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/bein-usa"
+  },
+  {
+    "name": "beIN Sports Francais 1",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/BeIN_Sports_logo.svg/1200px-BeIN_Sports_logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/bein-fr1"
+  },
+  {
+    "name": "beIN Sports Francais 2",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/BeIN_Sports_logo.svg/1200px-BeIN_Sports_logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/bein-fr2"
+  },
+  {
+    "name": "beIN Sports Francais 3",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/BeIN_Sports_logo.svg/1200px-BeIN_Sports_logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/bein-fr3"
+  },
+  {
+    "name": "Big Ten Network",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Big_Ten_Network_logo.svg/1200px-Big_Ten_Network_logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/bigten-usa"
+  },
+  {
+    "name": "CANAL+ Extra 1",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Canal%2B_Logo.svg/1200px-Canal%2B_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/canal-extra1"
+  },
+  {
+    "name": "CANAL+ Extra 2",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Canal%2B_Logo.svg/1200px-Canal%2B_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/canal-extra2"
+  },
+  {
+    "name": "CANAL+ Sport PL",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Canal%2B_Logo.svg/1200px-Canal%2B_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/canal-sport-pl"
+  },
+  {
+    "name": "CANAL+ Sport 2 PL",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Canal%2B_Logo.svg/1200px-Canal%2B_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/canal-sport2-pl"
+  },
+  {
+    "name": "CANAL+ Sport 3 PL",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Canal%2B_Logo.svg/1200px-Canal%2B_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/canal-sport3-pl"
+  },
+  {
+    "name": "CANAL+ Sport 4 PL",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Canal%2B_Logo.svg/1200px-Canal%2B_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/canal-sport4-pl"
+  },
+  {
+    "name": "CANAL+ Sport 5 PL",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Canal%2B_Logo.svg/1200px-Canal%2B_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/canal-sport5-pl"
+  },
+  {
+    "name": "CANAL+ Sport 6 PL",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Canal%2B_Logo.svg/1200px-Canal%2B_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/canal-sport6-pl"
+  },
+  {
+    "name": "CBS Sports Network",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/CBS_Sports_Network_logo.svg/1200px-CBS_Sports_Network_logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/cbssn-usa"
+  },
+  {
+    "name": "DAZN 1 Germany",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/DAZN_Logo.svg/1200px-DAZN_Logo.svg.png",
+    "genre": "Sports",
+    "url": "https://epiembeds.online/embed/dazn1-de"
+  }
 ];
